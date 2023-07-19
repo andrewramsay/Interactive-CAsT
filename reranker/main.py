f52a307028a9807a58b9c0bf2e763e156862b90f
Andrew Ramsay <andrewramsay@users.noreply.github.com>
2023-07-19 12:42:46 +0000

    increase max gRPC message size

--- reranker/main.py
+++ reranker/main.py
@@ -9,8 +9,14 @@ from concurrent import futures
 from rerankers import PygaggleReranker as RerankerServicer
 from reranker_pb2_grpc import add_RerankerServicer_to_server
 
+MAX_MESSAGE_LENGTH = 32 * 1024 * 1024
+opts = [
+    ('grpc.max_send_message_length', MAX_MESSAGE_LENGTH),
+    ('grpc.max_receive_message_length', MAX_MESSAGE_LENGTH),
+    ]
+
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10), options=opts)
     add_RerankerServicer_to_server(RerankerServicer(), server)
 
     server.add_insecure_port("[::]:8000")

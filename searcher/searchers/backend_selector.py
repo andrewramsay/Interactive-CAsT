54db17a0002b491d219caccca050b6a1cc36c8f2
Andrew Ramsay <andrewramsay@users.noreply.github.com>
2024-07-19 13:48:41 +0000

    Adding type annotions
    Also returning empty objects for invalid backend selections

--- searcher/searchers/backend_selector.py
+++ searcher/searchers/backend_selector.py
@@ -1,33 +1,45 @@
+from grpc import ServicerContext
+
 from .abstract_searcher import AbstractSearcher
 from .pyserini_searcher import PyseriniSearcher
 
-class BackendSelector(AbstractSearcher):
+from searcher_pb2 import SearchQuery, DocumentQuery, FulltextQuery
+from search_result_pb2 import SearchResult, Document, Passage, Fulltext
+from searcher_pb2_grpc import SearcherServicer
 
+
+class BackendSelector(AbstractSearcher):
     def __init__(self) -> None:
-        self.searchers = {
-            "PYSERINI" : PyseriniSearcher()
-        }
+        self.searchers = {"PYSERINI": PyseriniSearcher()}
 
         self.chosen_backend = None
-    
-    def search(self, search_query, context):
-        
+
+    def search(self, search_query: SearchQuery, context: ServicerContext) -> SearchResult:
         if search_query.search_backend == 0:
             self.chosen_backend = self.searchers["PYSERINI"]
-        
+        else:
+            return SearchResult()
+
         return self.chosen_backend.search(search_query, context)
-    
-    def get_document(self, document_request, context):
+
+    def get_document(self, document_query: DocumentQuery, context: ServicerContext) -> Document:
         # user might want to look up a doc directly without performing a
         # search first
 
         if self.chosen_backend:
-            return self.chosen_backend.get_document(document_request, context)
-        
+            return self.chosen_backend.get_document(document_query, context)
         else:
-            if document_request.search_backend == 0:
+            if document_query.search_backend == 0:
                 self.chosen_backend = self.searchers["PYSERINI"]
-            
-            return self.chosen_backend.get_document(document_request, context)
-        
-        
+            else:
+                return Document()
+
+            return self.chosen_backend.get_document(document_query, context)
+
+    def get_fulltext(self, fulltext_query: FulltextQuery, context: ServicerContext) -> Fulltext:
+        if fulltext_query.search_backend == 0:
+            self.chosen_backend = self.searchers["PYSERINI"]
+        else:
+            return Fulltext()
+
+        return self.chosen_backend.get_fulltext(fulltext_query, context)

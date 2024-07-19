ee4a862ca0bc37ef23e5a8ae3a114fbcc32d0121
Andrew Ramsay <andrewramsay@users.noreply.github.com>
2024-07-19 14:22:08 +0000

    Updating the pyserini searcher
    - Adding a new get_fulltext method for handling fulltext queries. This
      supports both retrieving a full record or a specific passage
    - Adding type annotations

--- searcher/searchers/pyserini_searcher.py
+++ searcher/searchers/pyserini_searcher.py
@@ -1,19 +1,24 @@
 import os
+import json
 
-from .abstract_searcher import AbstractSearcher
-from pyserini.search import LuceneSearcher
-from searcher_pb2 import SearchQuery, DocumentQuery
-from search_result_pb2 import SearchResult, Document, Passage
-
+from grpc import ServicerContext
 from bs4 import BeautifulSoup as bs
 import lxml
+from pyserini.search import LuceneSearcher
+
+from .abstract_searcher import AbstractSearcher
+from searcher_pb2 import SearchQuery, DocumentQuery, FulltextQuery
+from search_result_pb2 import SearchResult, Document, Passage, Fulltext
 
 class PyseriniSearcher(AbstractSearcher):
 
-    def __init__(self):
+    def __init__(self) -> None:
 
         self.index_paths = {
+            # this index contains the full text of all ClueWeb records
+            # in the iKAT collection
             'CLUEWEB' : '/shared/indexes/clueweb',
+            # this index contains the segmented passages
             'TRECiKAT2023' : '/shared/indexes/trec_ikat_2023'
             # new indices go here
         }
@@ -28,7 +33,7 @@ class PyseriniSearcher(AbstractSearcher):
 
         self.chosen_searcher = None
     
-    def search(self, search_query: SearchQuery, context):
+    def search(self, search_query: SearchQuery, context: ServicerContext) -> SearchResult:
 
         query: str = search_query.query
         num_hits: int = search_query.num_hits
@@ -38,7 +43,8 @@ class PyseriniSearcher(AbstractSearcher):
         elif search_query.search_parameters.collection == 1:
             self.chosen_searcher = self.indexes["TRECiKAT2023"]
         else:
-            pass # TODO
+            print("Unknown searcher selected!")
+            return SearchResult()
         
         bm25_b = search_query.search_parameters.parameters["b"]
         bm25_k1 = search_query.search_parameters.parameters["k1"]
@@ -55,38 +61,78 @@ class PyseriniSearcher(AbstractSearcher):
         return search_result
 
     
-    def get_document(self, document_query: DocumentQuery, context):
-
+    # TODO combine this method with get_fulltext?
+    def get_document(self, document_query: DocumentQuery, context: ServicerContext):
         document_id = document_query.document_id
         
-        # only ClueWeb in this version
         self.chosen_searcher = self.indexes['CLUEWEB']
 
         hit = self.chosen_searcher.doc(document_id)
-
-        retrieved_document = self.__convert_search_response(hit)
+        
+        content = json.loads(hit.raw())
+        retrieved_document = Document()
+        retrieved_document.id = hit.docid()
+        retrieved_document.url = content["url"]
+        passage = Passage()
+        passage.id = hit.docid().split(":")[1]
+        passage.body = content["contents"]
+        retrieved_document.passages.append(passage)
 
         return retrieved_document
 
-    
-    def __convert_search_response(self, hit):
+    def get_fulltext(self, fulltext_query: FulltextQuery, context: ServicerContext) -> Fulltext:
+        document_id = fulltext_query.document_id
+        passage_id = fulltext_query.passage_id
+
+        # if passage_id is < 0, it means this is a query for the fulltext
+        # of the selected record
+        index = self.indexes['CLUEWEB'] 
+        if passage_id != -1:
+            # searching for a specific passage instead
+            index = self.indexes["TRECiKAT2023"]
+
+        hit = index.doc(document_id)
+
+        fulltext = Fulltext()
+
+        # if we're returning a result from the fulltext index
+        if passage_id == -1:
+            data = json.loads(hit.raw())
+            fulltext.id = document_id
+            fulltext.url = data["url"]
+            fulltext.title = data["title"]
+            fulltext.body = data["contents"]
+            # TODO: forgot to remove title before indexing, can remove this once index rebuilt
+            fulltext.body = fulltext.body[fulltext.body.index("\n")+1:]
+        else:
+            # for the passage index, the contents are in trecweb format and need to pull out
+            # a selected passage
+            soup = bs(hit.raw(), "lxml")
+            fulltext.id = f"{document_id}:{passage_id}"
+            fulltext.url = soup.find("url").text
+            fulltext.title = soup.find("title").text
+            passages = soup.find_all("passage")
+            fulltext.body = passages[passage_id].text
+        
+        return fulltext
+
+    def __convert_search_response(self, hit) -> Document:
 
         retrieved_document = Document()
         soup = None
 
         try:
-            #This works if it is a regular search hit
+            # This works if it is a regular search hit
             soup = bs(hit.raw, "lxml")
             retrieved_document.id = hit.docid
             retrieved_document.score = hit.score
         except:
-            #This works if it is a document lookup
+            # This works if it is a document lookup
             soup = bs(hit.raw(), "lxml")
             retrieved_document.id = hit.docid()
         
         retrieved_document.url = soup.find("url").text
         retrieved_document.title = soup.find("title").text
-        
         
         passages = soup.find_all("passage")
         

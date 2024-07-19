ff8d83c069fd43e9c3de2d5760130d11d03da4f4
Andrew Ramsay <andrewramsay@users.noreply.github.com>
2024-07-19 13:35:58 +0000

    Adding type annotations and a get_fulltext method

--- searcher/searchers/abstract_searcher.py
+++ searcher/searchers/abstract_searcher.py
@@ -1,19 +1,29 @@
-from searcher_pb2_grpc import SearcherServicer
 from abc import ABC, abstractmethod
+from grpc import ServicerContext
+
+from searcher_pb2 import SearchQuery, DocumentQuery, FulltextQuery
+from search_result_pb2 import SearchResult, Document, Passage, Fulltext
+from searcher_pb2_grpc import SearcherServicer
+
 
 class AbstractSearcher(ABC, SearcherServicer):
-    
     @abstractmethod
-    def search(self, search_query, context):
+    def search(self, request: SearchQuery, context: ServicerContext) -> SearchResult:
         """
         Query an index and return search results
         """
-        pass
+        return SearchResult()
 
     @abstractmethod
-    def get_document(self, document_request, context):
+    def get_document(self, request: DocumentQuery, context: ServicerContext) -> Document:
         """
         Given a document id, return a document's attributes
         """
+        return Document()
 
-        pass
+    @abstractmethod
+    def get_fulltext(self, request: FulltextQuery, context: ServicerContext) -> Fulltext:
+        """
+        Given a ClueWeb-22 ID, return the document's full text (or a specific passage)
+        """
+        return Fulltext()
